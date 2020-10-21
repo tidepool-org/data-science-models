ee49e0cd095beb8c946a641587087f6f4eaa436e
Ed Nykaza <edward.t.nykaza@gmail.com>
2020-10-21 16:47:57 -0500

    feat: add in capability to allow a delay of 0

--- tidepool_data_science_models/models/icgm_sensor_generator_functions.py
+++ tidepool_data_science_models/models/icgm_sensor_generator_functions.py
@@ -241,10 +241,14 @@ def generate_icgm_sensors(
     #     iCGM = ((true_matrix + bias_matrix) * drift_multiplier) + noise
 
     # add delay or lag to the iCGM traces
-    delay_steps = np.int(np.round(delay / 5))
-    delayed_iCGM = np.insert(
-        values=iCGM[:, 0:1], obj=np.zeros(delay_steps, dtype=int), arr=iCGM[:, :-delay_steps], axis=1
-    )
+    if delay > 0:
+        delay_steps = np.int(np.round(delay / 5))
+        delayed_iCGM = np.insert(
+            values=iCGM[:, 0:1], obj=np.zeros(delay_steps, dtype=int), arr=iCGM[:, :-delay_steps], axis=1
+        )
+    else:
+        delayed_iCGM = iCGM
+
 
     # add spurious events to the trace
     # TODO: add in successive spurious events later, for now each spurious event is 5 minutes and can occur at any time
