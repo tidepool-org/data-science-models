49d90b8dc1c1ecc73c8bdcebd9afd915883d9703
Ed Nykaza <edward.t.nykaza@gmail.com>
2020-10-30 09:46:06 -0500

    test: add test to make sure sensor generation is repeatable

--- tests/test_sensor_generation.py
+++ tests/test_sensor_generation.py
@@ -1,6 +1,7 @@
+import pickle
 import numpy as np
 from pytest import approx
-from scipy.stats import norm, johnsonsu
+from scipy.stats import johnsonsu
 from tidepool_data_science_models.models.icgm_sensor_generator import iCGMSensorGenerator
 from tidepool_data_science_models.models.icgm_sensor_generator_functions import create_dataset
 
@@ -83,26 +84,18 @@ def test_that_fit_icgm_sensor_has_correct_stats():
         # NOTE: if you want to see how this works, see the plots commented below
         bias_drift_range_start = sensor_properties.loc[0, "bias_drift_range_start"].values[0]
         bias_drift_range_end = sensor_properties.loc[0, "bias_drift_range_end"].values[0]
-        for phi in np.arange(-np.pi, np.pi, np.pi/2):
-            for bias_drift_oscillations in [1/32, 1, 2]:
+        for phi in np.arange(-np.pi, np.pi, np.pi / 2):
+            for bias_drift_oscillations in [1 / 32, 1, 2]:
                 t = np.linspace(0, (bias_drift_oscillations * np.pi), 2880)
                 sn = np.sin(t + phi)
-                drift_multiplier = np.interp(
-                    sn,
-                    (-1, 1),
-                    (
-                        bias_drift_range_start,
-                        bias_drift_range_end,
-                    ),
-                )
+                drift_multiplier = np.interp(sn, (-1, 1), (bias_drift_range_start, bias_drift_range_end,),)
 
                 drift_multiplier_min = drift_multiplier.min()
                 drift_multiplier_max = drift_multiplier.max()
                 if bias_drift_oscillations < 2:
-                    assert (
-                        (drift_multiplier_min >= bias_drift_range_start)
-                        and (drift_multiplier_max <= bias_drift_range_end)
-                     )
+                    assert (drift_multiplier_min >= bias_drift_range_start) and (
+                        drift_multiplier_max <= bias_drift_range_end
+                    )
                 else:
                     assert bias_drift_range_start == approx(drift_multiplier_min, abs=1e-2)
                     assert bias_drift_range_end == approx(drift_multiplier_max, abs=1e-2)
@@ -112,3 +105,38 @@ def test_that_fit_icgm_sensor_has_correct_stats():
                 # plt.title("Bias Drift Applied with phi={:.2f}, oscillations={}".format(phi, bias_drift_oscillations))
                 # plt.show()
 
+
+def test_that_results_are_repeatable():
+    """
+    check that fit gives the same exact results as the fit to the benchmark dataset
+    see (/notebooks/make_benchmark_datasets.py for details on the benchmark dataset)
+    """
+
+    benchmark_sensor_generator_obj = pickle.load(open("benchmark_results_2020_10_30.pkl", "rb"))
+
+    batch_size = 3
+    random_seed = 0
+    new_sensor_generator = iCGMSensorGenerator(
+        batch_training_size=batch_size,
+        use_g6_accuracy_in_loss=False,
+        bias_type="percentage_of_value",
+        bias_drift_type="random",
+        random_seed=random_seed,
+        verbose=False,
+    )
+
+    # refit to the same benchmark true bg dataset
+    new_sensor_generator.fit(benchmark_sensor_generator_obj.true_bg_trace)
+
+    assert new_sensor_generator.sc_thresholds == benchmark_sensor_generator_obj.sc_thresholds
+    assert new_sensor_generator.batch_training_size == benchmark_sensor_generator_obj.batch_training_size
+    assert (
+        new_sensor_generator.johnson_parameter_search_range
+        == benchmark_sensor_generator_obj.johnson_parameter_search_range
+    )
+    assert new_sensor_generator.individual_sensor_properties.equals(
+        benchmark_sensor_generator_obj.individual_sensor_properties
+    )
+    assert new_sensor_generator.icgm_special_controls_accuracy_table.equals(
+        benchmark_sensor_generator_obj.icgm_special_controls_accuracy_table
+    )
